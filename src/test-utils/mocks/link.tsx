c882ee58bdf0b377a07e2d1eed950d904833e3ce
Shershneva Kseniya <megacoolksuha@mail.ru>
2025-09-03 13:15:59 +0300

    chore: add next.js, configure vitest

---
+++ src/test-utils/mocks/link.tsx
@@ -0,0 +1,8 @@
+import "@testing-library/jest-dom";
+import { vi } from "vitest";
+
+vi.mock("next/link", () => ({
+  default: ({ href, children }: { href: string; children: React.ReactNode }) => (
+    <a href={href}>{children}</a>
+  ),
+}));
