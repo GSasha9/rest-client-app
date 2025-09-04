2d7d50245997831205badaed7bd434313c03e8b5
Sasha Hurbanava <sasha.hurbnva@gmail.com>
2025-09-04 15:05:29 +0300

    feat: add Material UI library and initialize Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
-import Image from 'next/image';
-import Page from '../pageComponents/page';
-import logo from '../assets/icon.svg';
+import HomePage from '@/pageComponents/home-page/home-page';
+import { CssBaseline } from '@mui/material';
 
 export default function Home() {
   return (
-    <div>
-      <header>
-        <Image src={logo} alt={'icon'} width={70} height={70}></Image>
-      </header>
-      <main>
-        <Page />
-      </main>
-    </div>
+    <>
+      <CssBaseline />
+      <div>
+        <header>
+          <span>Header</span>
+        </header>
+        <main>
+          <HomePage />
+        </main>
+        <footer>Footer</footer>
+      </div>
+    </>
   );
 }
