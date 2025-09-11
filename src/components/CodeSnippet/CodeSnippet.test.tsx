0e49dce95232e4245b983cf7ddf6503de3bfc6a4
Sasha Hurbanava <sasha.hurbnva@gmail.com>
2025-09-11 11:04:38 +0300

    test: add CodeSnippet tests

---
+++ src/components/CodeSnippet/CodeSnippet.test.tsx
@@ -0,0 +1,51 @@
+import { describe, it, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import CodeSnippet from './CodeSnippet';
+import userEvent from '@testing-library/user-event';
+
+describe('CodeSnippet', () => {
+  const mockData = {
+    url: 'test.com',
+    method: 'POST',
+    header: [{ key: 'headerKey', value: 'headerValue' }],
+    body: 'testBody',
+  };
+  it('renders tabs correctly', () => {
+    render(<CodeSnippet data={mockData} />);
+
+    const tabs = screen.getAllByRole('tab');
+    expect(tabs.map((t) => t.textContent)).toEqual(
+      expect.arrayContaining([
+        'Java Script (Fetch API)',
+        'Java Script (XHR)',
+        'curl',
+        'NodeJS',
+        'Python',
+        'Java',
+        'C#',
+        'Go',
+      ])
+    );
+    expect(screen.getAllByRole('tab').length).toEqual(9);
+  });
+
+  it('clicking a tab generates code', async () => {
+    render(<CodeSnippet data={mockData} />);
+
+    expect(screen.getByRole('tab', { selected: true })).toHaveTextContent('');
+
+    const curlTab = screen.getByRole('tab', { name: /curl/i });
+
+    await userEvent.click(curlTab);
+
+    expect(curlTab).toHaveAttribute('aria-selected', 'true');
+    expect(screen.getByRole('textbox')).toHaveTextContent('curl');
+
+    const emptyTab = screen.getByRole('tab', { name: '' });
+
+    await userEvent.click(emptyTab);
+
+    expect(curlTab).toHaveAttribute('aria-selected', 'true');
+    expect(screen.getByRole('textbox')).toHaveTextContent('curl');
+  });
+});
