30e477f3ff1d6db87873cd3bbb667c812b0502bc
Shershneva Kseniya <megacoolksuha@mail.ru>
2025-09-12 15:45:34 +0300

    feat: implement a hook that gets data from a url and writes it to a url

---
+++ src/hooks/restful-url.ts
@@ -0,0 +1,35 @@
+'use client';
+
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { useMemo } from 'react';
+import { HeaderItem, Methods } from '../models/rest-client';
+
+interface RestData {
+  method: Methods;
+  headers: HeaderItem[];
+  url: string;
+  body?: string;
+}
+
+export const useRestfulUrl = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const data = useMemo(
+    () => JSON.parse(atob(searchParams.get('data') || '') || '""') as RestData,
+    [searchParams]
+  );
+
+  const setSearchParams = (data: Partial<RestData>) => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    params.set('data', btoa(JSON.stringify(data)));
+    const query = params.toString();
+
+    router.push(pathname + (query ? '?' + query : ''));
+  };
+
+  return [data, setSearchParams] as const;
+};
