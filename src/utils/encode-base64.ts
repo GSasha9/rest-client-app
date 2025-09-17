7a23b85251c6c73d412d5c1575579c86585d4d5a
Shershneva Kseniya <megacoolksuha@mail.ru>
2025-09-17 22:46:47 +0300

    fix: change conversion to base64

--- src/utils/encode-base64.ts
+++ src/utils/encode-base64.ts
@@ -1,7 +1,5 @@
 export function encodeBase64(str: string) {
-  return btoa(
-    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (_match, p1) =>
-      String.fromCharCode(parseInt(p1, 16))
-    )
-  );
+  const result = Buffer.from(str, 'utf-8').toString('base64');
+
+  return encodeURIComponent(result);
 }
